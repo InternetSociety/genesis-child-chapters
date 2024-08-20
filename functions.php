506575678b46db377feb7f7f66a6c91d3b6f3f5e
root <root@chapter-template.isoc.org>
2024-08-20 22:14:29 +0000

    Fixing php notice

--- functions.php
+++ functions.php
@@ -707,10 +707,9 @@ function av_ln_shortcode($atts){
     $list = '<div class="latest-posts">';
     while(have_posts()) : the_post();
 
-//if ( $tag_list && ! is_wp_error( $tag_list ) ) {
-//    $tag_list;
-//}
-        $featured_img_url = get_the_post_thumbnail_url($post->ID, 'full');
+		$post_ID = (empty($post->ID)) ? '' : $post->ID;
+        $featured_img_url = get_the_post_thumbnail_url($post_ID, 'full');
+  
         $list .= '
 
         <div class="latest-blog-post rel-box">
