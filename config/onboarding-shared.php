6483443f2fd788bf9d7c13f16456b602dfbb6bc3
root <root@chapter-template.isoc.org>
2024-08-21 14:59:18 +0000

    Committing next child-theme changes

--- config/onboarding-shared.php
+++ config/onboarding-shared.php
@@ -157,18 +157,63 @@ return [
 				'type' => 'text',
 				'args' => [
 					'title'  => '',
-					'text'   => '<p>Footer Col One Goes here, Logo and Social Icons.<p>',
+					'text'   => '<section id="simple-social-icons-2" class="widget simple-social-icons">
+                    <ul class="aligncenter">
+                        <li class="ssi-facebook">
+                            <a href="https://www.facebook.com/InternetSociety" target="_blank" rel="noopener noreferrer">
+                            <img width="30" height="30" src="/wp-content/themes/genesis-child-chapters/config/import/images/social/white/facebook.svg">
+                            </a>
+                        </li>
+                        <li class="ssi-instagram">
+                            <a href="https://www.instagram.com/internetsociety/" target="_blank" rel="noopener noreferrer">
+                            <img width="30" height="30" src="/wp-content/themes/genesis-child-chapters/config/import/images/social/white/instagram.svg">
+                            </a>
+                        </li>
+                        <li class="ssi-linkedin">
+                            <a href="https://www.linkedin.com/company/internet-society/" target="_blank" rel="noopener noreferrer">
+                            <img width="30" height="30" src="/wp-content/themes/genesis-child-chapters/config/import/images/social/white/linkedin.svg">
+                            </a>
+                            </li>
+                        <li class="ssi-rss">
+                            <a href="https://www.internetsociety.org/feed/" target="_blank" rel="noopener noreferrer">
+                            <img width="30" height="30" src="/wp-content/themes/genesis-child-chapters/config/import/images/social/white/rss.svg">
+                            </a>
+                        </li>
+                        <li class="ssi-twitter">
+                            <a href="https://twitter.com/internetsociety" target="_blank" rel="noopener noreferrer">
+                            <img width="30" height="30" src="/wp-content/themes/genesis-child-chapters/config/import/images/social/white/X.svg"></a>
+                        </li>
+                        <li class="ssi-youtube"><a href="https://www.youtube.com/user/InternetSocietyVideo" target="_blank" rel="noopener noreferrer">
+                            <img width="30" height="30" src="/wp-content/themes/genesis-child-chapters/config/import/images/social/white/youtube.svg"></a>
+                        </li>
+                    </ul>
+                    </section>',
 					'filter' => 1,
-					'visual' => 1,
+					'visual' => 0,
 				],
 			],
+        ],
+
+        'footer-2' => [
+            [
+                'type' => 'text',
+                'args' => [
+                    'title'  => '',
+                    'text'   => '<section id="media_image-2" class="widget widget_media_image"><div class="widget-wrap"><a href="https://assets.internetsociety.org/%20style%20guide/#page/F92B2B59-A837-42C4-BCA14EEBF43CD20BURL"><img width="300" height="300" src="/wp-content/uploads/2024/01/internet-society-logo-header-233px-47.svg" class="image wp-image-838  attachment-medium size-medium" alt="Internet Society Logo Header 233px 47" style="max-width: 100%; height: auto;" decoding="async" loading="lazy" srcset="https://chapter-template.internetsociety.org/wp-content/uploads/2024/01/internet-society-logo-header-233px-47.svg 150w, https://chapter-template.internetsociety.org/wp-content/uploads/2024/01/internet-society-logo-header-233px-47.svg 300w, https://chapter-template.internetsociety.org/wp-content/uploads/2024/01/internet-society-logo-header-233px-47.svg 1024w, https://chapter-template.internetsociety.org/wp-content/uploads/2024/01/internet-society-logo-header-233px-47.svg 1536w, https://chapter-template.internetsociety.org/wp-content/uploads/2024/01/internet-society-logo-header-233px-47.svg 2048w, https://chapter-template.internetsociety.org/wp-content/uploads/2024/01/internet-society-logo-header-233px-47.svg 750w, https://chapter-template.internetsociety.org/wp-content/uploads/2024/01/internet-society-logo-header-233px-47.svg 600w" sizes="(max-width: 300px) 100vw, 300px"></a></div></section>',
+                    'filter' => 0,
+                    'visual' => 0,
+                ],
+            ],
 		],
         'footer-copyright' => [
             [
                 'type' => 'text',
                 'args' => [
                     'title'  => '',
-                    'text'   => '<p>Footer Copyright One Goes Here.<p>',
+                    'text'   => '<p class="footer-address">Physical Address
+                                of the Chapter
+                                +1-111-111-111</p>
+                                <p class="footer-copy"><small>© [year] Internet Society Chapter</small></p>',
                     'filter' => 1,
                     'visual' => 1,
                 ],
