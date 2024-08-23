5119ee6fe2b89f106090781039c5d3ce34f95c92
root <root@chapter-template.isoc.org>
2024-08-23 10:13:24 +0000

    Small content import update

--- config/import/content/home-color.php
+++ config/import/content/home-color.php
@@ -14,8 +14,8 @@ $genesis_child_chapters_homepage_header_image_url = CHILD_URL . '/config/import/
 
 return <<<CONTENT
 <!-- wp:group {"className":"inner-page-header","layout":{"type":"constrained"}} -->
-<div class="wp-block-group inner-page-header"><!-- wp:cover {"url":"https://chapter-template.isoc.org/wp-content/themes/genesis-child-chapters/config/import/images/about/Green-pattern-header.jpg","id":359,"alt":"Green Pattern Header","dimRatio":0,"minHeight":400,"minHeightUnit":"px","isDark":false,"className":"white-overlay"} -->
-<div class="wp-block-cover is-light white-overlay" style="min-height:400px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-359" alt="Green Pattern Header" src="https://chapter-template.isoc.org/wp-content/themes/genesis-child-chapters/config/import/images/about/Green-pattern-header.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:heading {"level":1} -->
+<div class="wp-block-group inner-page-header"><!-- wp:cover {"url":"https://chapter-template.isoc.org/wp-content/themes/genesis-child-chapters-main/config/import/images/about/Green-pattern-header.jpg","id":359,"alt":"Green Pattern Header","dimRatio":0,"minHeight":400,"minHeightUnit":"px","isDark":false,"className":"white-overlay"} -->
+<div class="wp-block-cover is-light white-overlay" style="min-height:400px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-359" alt="Green Pattern Header" src="https://chapter-template.isoc.org/wp-content/themes/genesis-child-chapters-main/config/import/images/about/Green-pattern-header.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:heading {"level":1} -->
 <h1 class="wp-block-heading">Wordpress theme for Internet Society Chapters</h1>
 <!-- /wp:heading -->
 
