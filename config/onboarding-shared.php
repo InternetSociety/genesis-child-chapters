0e4efd2bb6825a5efe3bf62a8633eabe7ce896d0
root <root@chapter-template.isoc.org>
2024-08-27 21:21:06 +0000

    Tweaking further search form 3

--- config/onboarding-shared.php
+++ config/onboarding-shared.php
@@ -127,7 +127,7 @@ return [
                 'type' => 'text',
                 'args' => [
                     'title'  => '',
-                    'text'   => '<div class="sec-nav"><a class="search-toggle" role="button"><span class="sr-only">Search Dropdown</span><img class="search-ico" src="/wp-content/themes/genesis-child-chapters-main/images/search-icon-21px-21px.svg" alt="Search Icon" width="21" height="21" /></a><a class="join customize-unpreviewable" href="https://community.internetsociety.org/s/new-registration">Join</a></div><div class="search-block" style="display: none;"><form class="is-search-form" role="search" action="/" method="get" target="_self"><input name="search" type="hidden" /><label for="is-search-input-540"><input id="is-search-input-540" class="is-search-input" autocomplete="off" name="s" type="search" value="" placeholder="Search by Keyword" /></label><input name="id" type="hidden" value="540" /></form></div>',
+                    'text'   => '<div class="sec-nav"><a class="search-toggle" role="button"><span class="sr-only">Search Dropdown</span><img class="search-ico" src="/wp-content/themes/genesis-child-chapters-main/images/search-icon-21px-21px.svg" alt="Search Icon" width="21" height="21" /></a><a class="join customize-unpreviewable" href="https://community.internetsociety.org/s/new-registration">Join</a></div><div class="search-block" style="display: none;"><form class="is-search-form" role="search" action="/" method="get" target="_self"><label for="is-search-input-540"><input id="is-search-input-540" class="is-search-input" autocomplete="off" name="s" type="search" value="" placeholder="Search by Keyword" /></label></form></div>',
                     'filter' => 1,
                     'visual' => 1,
                 ],
