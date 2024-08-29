01a6c7484da71ec613d26155e60c08fb90e381c4
root <root@chapter-template.isoc.org>
2024-08-29 16:22:16 +0000

    Re-shuffling import contents

--- config/import/content/installation.php
+++ config/import/content/installation.php
@@ -16,12 +16,8 @@ return <<<CONTENT
 <!-- wp:group {"className":"inner-page-header","layout":{"type":"constrained"}} -->
 <div class="wp-block-group inner-page-header"><!-- wp:cover {"url":"https://chapter-template.isoc.org/wp-content/themes/genesis-child-chapters-main/config/import/images/about/Green-pattern-header.jpg","id":359,"alt":"Green Pattern Header","dimRatio":0,"minHeight":400,"minHeightUnit":"px","isDark":false,"className":"white-overlay"} -->
 <div class="wp-block-cover is-light white-overlay" style="min-height:400px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-359" alt="Green Pattern Header" src="https://chapter-template.isoc.org/wp-content/themes/genesis-child-chapters-main/config/import/images/about/Green-pattern-header.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:heading {"level":1} -->
-<h1 class="wp-block-heading">Wordpress theme for Internet Society Chapters</h1>
-<!-- /wp:heading -->
-
-<!-- wp:paragraph {"className":"inner-header-cover"} -->
-<p class="inner-header-cover">This is the WordPress theme used by Internet Society chapters.</p>
-<!-- /wp:paragraph --></div></div>
+<h1 class="wp-block-heading">Installation</h1>
+<!-- /wp:heading --></div></div>
 <!-- /wp:cover --></div>
 <!-- /wp:group -->
 
